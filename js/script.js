01285d63b6d22dc6ea09ffa4f39b78b163fc95fe
Vladimir <77880485+kudashevvn@users.noreply.github.com>
2021-09-01 17:44:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -234,3 +234,35 @@ for (let link of links){
 		})
 	})
 }
+
+// Анимания отзывов
+let animItems = document.querySelectorAll('._anim-items');
+if (animItems.length > 0){
+	window.addEventListener('scroll', animScroll);
+	function animScroll(params){
+		for (let i = 0; i < animItems.length; i++){
+			let animItem = animItems[i];
+			let animItemHeight = animItem.offsetHeight;
+			let animItemOffset = offset(animItem).top;
+			let animStart = 5;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+				animItem.classList.add('_active');
+			} else {
+				animItem.classList.remove('_active');
+			}
+
+		}
+	}
+	function offset(el){
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+} 
+animScroll();
